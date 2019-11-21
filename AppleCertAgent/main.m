df1131a9a5c9bc9a306d887e84aa17aa7a249646
AppleChianWalker <58024833+AppleChianWalker@users.noreply.github.com>
2019-11-21 20:36:00 +0800

    Initial commit

---
+++ AppleCertAgent/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppleCertAgent
+//
+//  Created by no178 on 2019/11/21.
+//  Copyright © 2019 no178. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AltSign/AltSign.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
